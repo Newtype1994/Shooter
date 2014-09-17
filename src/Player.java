ebee5da745a92f873c3d717ae08e28873107fe9c
Atibet <Atibet@Atibet-PC>
2014-09-17 09:04:02 +0700

    Start check collision

--- src/Player.java
+++ src/Player.java
@@ -7,15 +7,49 @@ public class Player {
 		private Image image;
 		private int x;
 		private int y;
+		private int rotation;
 		
 		public Player(int x,int y)throws SlickException {
 			image = new Image("res/Tank.png");
 			this.x = x;
 			this.y = y;
+			this.rotation = 0;
 		}
 		
 		public void draw(){
 			image.draw(x,y);
+		}
+		
+		public void move(int x,int y){
+			if(this.x + x > 0 && this.x + x < 608){
+				this.x += x;
+			}
+			if(this.x + x > 0 && this.x + x < 608){
+				this.x += x;
+			}
+			if(this.x + x > 0 && this.x + x < 608){
+				this.x += x;
+			}
+			if(this.y + y > 0 && this.y + y < 448){
+				this.y += y;
+			}
+			if(this.y + y > 0 && this.y + y < 448){
+				this.y += y;
+			}
+			if(this.y + y > 0 && this.y + y < 448){
+				this.y += y;
+			}
+			if(x != 0 )this.rotation = (int)Math.toDegrees(Math.acos(x))+90;
+			else this.rotation = (int)Math.toDegrees(Math.asin(y))+90;
+			image.setRotation(rotation);
+		}
+		
+		public void shoot(Bullet[] bullet){
+			for(int i = 0 ; i < 3 ; i++){
+				if(bullet[i].use(x,y,rotation)){
+					break;
+				}
+			}
 		}
 		
 }
--- src/ShooterGame.java
+++ src/ShooterGame.java
@@ -10,6 +10,9 @@ public class ShooterGame extends BasicGame{
 	
 	private Player player1;
 	private Player player2;
+	private Bullet[] bullet1 = new Bullet[3];
+	private Bullet[] bullet2 = new Bullet[3];
+	private int[][] map = new int[640][480];
 
 	public ShooterGame(String title) {
 		super(title);
@@ -20,24 +23,73 @@ public class ShooterGame extends BasicGame{
 	public void render(GameContainer container, Graphics g) throws SlickException {
 		player1.draw();
 		player2.draw();
-		
+		for(int i = 0 ; i < 3 ; i++){
+			bullet1[i].draw();
+			bullet2[i].draw();
+		}
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		player1 = new Player(0,240);
+		map[0][240] = 1;
 		player2 = new Player(608,240);
-		
+		map[608][240] = 2;
+		for(int i = 0 ; i < 3 ; i++){
+			bullet1[i] = new Bullet(-100,-100);
+			bullet2[i] = new Bullet(-100,-100);
+		}
 	}
 	
-	void updatePlayerMovement(Input input,int delta){
-		
+	@Override
+	public void keyPressed (int key , char c){
+		if(key == Input.KEY_G){
+			player1.shoot(bullet1);
+		}
+		if(key == Input.KEY_ENTER){
+			player2.shoot(bullet2);
+		}
 	}
-
+	
+	void updatePlayer1Movement(Input input,int delta){
+		if(input.isKeyDown(Input.KEY_W)){
+			player1.move(0,-1);
+		}
+		else if(input.isKeyDown(Input.KEY_S)){
+			player1.move(0,1);
+		}
+		else if(input.isKeyDown(Input.KEY_A)){
+			player1.move(-1,0);
+		}
+		else if(input.isKeyDown(Input.KEY_D)){
+			player1.move(1,0);
+		}
+	}
+	
+	void updatePlayer2Movement(Input input,int delta) throws SlickException{
+		if(input.isKeyDown(Input.KEY_UP)){
+			player2.move(0,-1);
+		}
+		else if(input.isKeyDown(Input.KEY_DOWN)){
+			player2.move(0,1);
+		}
+		else if(input.isKeyDown(Input.KEY_LEFT)){
+			player2.move(-1,0);
+		}
+		else if(input.isKeyDown(Input.KEY_RIGHT)){
+			player2.move(1,0);
+		}
+	}
+	
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
 		Input input = container.getInput();
-		updatePlayerMovement(input,delta);
+		updatePlayer1Movement(input,delta);
+		updatePlayer2Movement(input,delta);
+		for(int i=0;i<3;i++){
+			bullet1[i].update();
+			bullet2[i].update();
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -45,7 +97,8 @@ public class ShooterGame extends BasicGame{
 	      ShooterGame game = new ShooterGame("Flappy Dot");
 	      AppGameContainer appgc = new AppGameContainer(game);
 	      appgc.setDisplayMode(640,480, false);
-	      appgc.setMinimumLogicUpdateInterval(1000/30);
+	      appgc.setTargetFrameRate(60);
+	      //appgc.setMinimumLogicUpdateInterval(1000/30);
 	      appgc.start();
 	    } catch (SlickException e) {
 	      e.printStackTrace();
